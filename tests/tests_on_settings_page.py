c101520252f6ec7fe1871110ebcf3d4042cf1c8d
mskib77 <mskib77@gmail.com>
2021-04-14 14:56:55 +0200

    addint test below lower linmit

--- tests/tests_on_settings_page.py
+++ tests/tests_on_settings_page.py
@@ -1,10 +1,12 @@
+from time import sleep
+
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from locators import MainActivityLocators, SettingsAcctivityLocators
 from tests.base_test import BaseTest
 
-class SettingsPageTest(BaseTest):
 
+class SettingsPageTest(BaseTest):
     # difficulty levels
     MAX_LEVEL = 6
     MIN_LEVEL = 1
@@ -19,14 +21,29 @@ class SettingsPageTest(BaseTest):
         self.ma.long_touch_on_image()
         WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(SettingsAcctivityLocators.POZIOM))
 
-    def test_increase_level_beyond_upper_limit(self):
+    def test_increase_level_above_upper_limit(self):
         maxl = SettingsPageTest.MAX_LEVEL
         bplus = self.sa.get_bplus_button()
         curr_level = int(self.sa.get_poziom_view().text)
         # Trying to exceed MAX_LEVEL:
-        for i in (curr_level, maxl + 1):
+        for i in range(curr_level, maxl + 1):
             bplus.click()
+            sleep(0.5)
         curr_level = int(self.sa.get_poziom_view().text)
-        # print(curr_level)
         self.assertTrue(curr_level == maxl, "Maximum difficulty level set beyond allowed limit!")
 
+    def test_decrease_level_below_lower_limit(self):
+        minl = SettingsPageTest.MIN_LEVEL
+        bminus = self.sa.get_bminus_button()
+        curr_level = int(self.sa.get_poziom_view().text)
+        # Trying to go below MIN_LEVEL:
+        for i in range(curr_level, minl - 1, -1):
+            bminus.click()
+            sleep(0.5)
+        curr_level = int(self.sa.get_poziom_view().text)
+        self.assertTrue(curr_level == minl, "Minimum difficulty level set below allowed limit!")
+
+    def test_numbers_of_buttons_equals_difficulty_level(self):
+        """
+
+        """
