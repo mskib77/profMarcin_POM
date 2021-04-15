6c76c2fae589b7e1ea0af2835ed534e2c489d6e6
mskib77 <mskib77@gmail.com>
2021-04-15 15:08:53 +0200

    difficulty level tests - in progress

--- tests/tests_on_settings_page.py
+++ tests/tests_on_settings_page.py
@@ -1,5 +1,6 @@
+import unittest
 from time import sleep
-
+from random import randint
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from locators import MainActivityLocators, SettingsAcctivityLocators
@@ -21,6 +22,7 @@ class SettingsPageTest(BaseTest):
         self.ma.long_touch_on_image()
         WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(SettingsAcctivityLocators.POZIOM))
 
+    @unittest.skip
     def test_increase_level_above_upper_limit(self):
         maxl = SettingsPageTest.MAX_LEVEL
         bplus = self.sa.get_bplus_button()
@@ -32,6 +34,7 @@ class SettingsPageTest(BaseTest):
         curr_level = int(self.sa.get_poziom_view().text)
         self.assertTrue(curr_level == maxl, "Maximum difficulty level set beyond allowed limit!")
 
+    @unittest.skip
     def test_decrease_level_below_lower_limit(self):
         minl = SettingsPageTest.MIN_LEVEL
         bminus = self.sa.get_bminus_button()
@@ -45,5 +48,53 @@ class SettingsPageTest(BaseTest):
 
     def test_number_of_buttons_equals_difficulty_level(self):
         """
+        Sets the number of buttons to a random value between MIN_LEVEL and MAX_LEVEL (both included)
+        Then checks if there appear the same number of buttons on MainActivity
+        The proces is repeated N-times for better testing
         """
+        N = 3
+        for i in range(0,N):
+           self.__perform_difficulty_change_checking()
+           self.tearDown()
+           super().setUp()
+           sleep(3)
+           self.__go_to_settings_page()
+
+
+
+    def __perform_difficulty_change_checking(self):
+
+        maxl = SettingsPageTest.MAX_LEVEL
+        minl = SettingsPageTest.MIN_LEVEL
+
+        bminus = self.sa.get_bminus_button()
+        bplus = self.sa.get_bminus_button()
+        curr_level = int(self.sa.get_poziom_view().text)
+
+        # Setting randowm dificulty level:
+        while True:  # ensures the generated random number is different from what we initially see on the screen
+            rnd_level = randint(1, 6)
+            if rnd_level != curr_level: break
+        delta = curr_level - rnd_level
+        if delta > 0:
+            klawisz = self.sa.get_bminus_button()
+        else:
+            klawisz = self.sa.get_bplus_button()
+
+        print("rnd_level = ",rnd_level)
+        print(f"delta: {delta}")
+        print("naciskam klawisz ze znakiem ",klawisz.text )
+        for i in range(0, abs(delta)):
+            klawisz.click()
+            sleep(0.5)
+
+        # Going back to MainActivity and checking the number of buttons:
+        self.driver.back()
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(MainActivityLocators.WORD_BUTTONS_LIST))
+        wb_list = self.ma.get_word_buttons_list()
+        wb_count = len(wb_list)
+        sleep(2)
+        self.assertTrue(rnd_level == wb_count, "Poziom trudnosci i liczba buttonow nie zgadzają się!")
+
+
 
